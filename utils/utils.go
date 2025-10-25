af446ebd82ef4b5dbe10b41c514af33f611ee25f
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2025-10-25 16:24:21 -0500

    use max and min.

--- utils/utils.go
+++ utils/utils.go
@@ -8,8 +8,5 @@ const RunningInWASM = runtime.GOARCH == "wasm"
 
 // https://en.wikipedia.org/wiki/Absolute_difference
 func AbsDiffInt(x, y int) int {
-	if x < y {
-		return y - x
-	}
-	return x - y
+	return max(x, y) - min(x, y)
 }
